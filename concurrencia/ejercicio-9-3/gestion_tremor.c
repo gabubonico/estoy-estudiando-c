a69d1cd90030a64d079e22e8086cdb34e5ae042f
gabubonico <gabriel.cortes.002@gmail.com>
2022-03-09 12:29:35 +0100

    metodo no me acuerdo cual la verdad

--- concurrencia/ejercicio-9-3/gestion_tremor.c
+++ concurrencia/ejercicio-9-3/gestion_tremor.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "gestion_tremor.h"
 
 /* Muestra los episodios de tremor que ha tenido el usuario por orden cronológico, primero los más nuevos, se le pasa la cabeza de la lista  */
@@ -16,14 +17,23 @@
 Registra un episodio de tremor, con su fecha y duración, OK es igual a 1 si se puede, 0 si no es posible pedir memoria
  */
 	void registrar(T_Lista * ptr_lista_head, const time_t * fecha, unsigned duracion,unsigned* ok){
-        ;
+       // TODO 
     }
 
 /* 
 Libera todos los episodios que son anteriores a la fecha dada. Devuelve el número que se ha eliminado.
  */
 	int liberar(T_Lista * ptr_lista_head, const time_t *  fecha){
-        return 0;
+		int cont = 0;
+		struct T_Nodo *actual = *ptr_lista_head;
+		struct T_Nodo *sig;
+		while (sig->fecha != fecha) {
+			sig = actual->sig;
+			free(actual);
+			actual = sig;
+			cont++;
+		}
+		return cont;
     }
 
 /* Destruye la estructura utilizada (libera todos los nodos de la lista. El parámetro manejador debe terminar apuntando a NULL */
