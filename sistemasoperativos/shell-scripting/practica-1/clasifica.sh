2c9b9ea39467e120fdd1fa259da399f723c58aff
gabubonico <gabriel.cortes.002@gmail.com>
2022-03-21 11:49:05 +0100

    clasifica.sh done

---
+++ sistemasoperativos/shell-scripting/practica-1/clasifica.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if  [ ! $(ls $2) ]
+then
+	mkdir --parents $2
+fi
+
+for i in $1/*
+do
+	artista=$(cat $i | grep "autor" | cut -d ":" -f 2)
+	cancion=$(cat $i | grep "titulo" | cut -d ":" -f 2)
+
+	if  [ ! $(ls $2/$artista) ]
+	then
+		mkdir --parents $2/$artista
+	fi
+
+	cp $i $2/$artista/$cancion.mp4
+done
