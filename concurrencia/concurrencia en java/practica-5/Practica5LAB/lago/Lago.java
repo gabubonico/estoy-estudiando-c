25c025c39b4a6c52246e162f244ab98fc0e4c6de
gabubonico <gabriel.cortes.002@gmail.com>
2022-04-29 14:00:45 +0200

    practica 5 concurrencia lago

---
+++ concurrencia/concurrencia en java/practica-5/Practica5LAB/lago/Lago.java
@@ -0,0 +1,50 @@
+package lago;
+
+public class Lago {
+	private volatile int nivel = 0;
+	private boolean hayDato = false;
+
+	public Lago(int valorInicial) {
+		nivel = valorInicial;
+	}
+
+	// f0IncDec, f0Inc
+	public void incRio0() {
+		while (hayDato) {
+			Thread.yield();
+		}
+		nivel++;
+		hayDato = true;
+	}
+
+	// f0IncDec, f1Inc
+	public void incRio1() {
+		while (hayDato) {
+			Thread.yield();
+		}
+		nivel++;
+		hayDato = true;
+	}
+
+	// f1IncDec, f0Dec
+	public void decPresa0() {
+		while (!hayDato) {
+			Thread.yield();
+		}
+		nivel--;
+		hayDato = false;
+	}
+
+	// f1IncDec, f1Dec
+	public void decPresa1() {
+		while (!hayDato) {
+			Thread.yield();
+		}
+		nivel--;
+		hayDato = false;
+	}
+
+	public int nivel() {
+		return nivel;
+	}
+}
